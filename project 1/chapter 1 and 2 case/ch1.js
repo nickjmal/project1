322c0572027220d3d1a0152047695932c1985540
nickjmal <78528974+nickjmal@users.noreply.github.com>
2021-02-03 23:35:08 -0800

    Add files via upload

---
+++ project 1/chapter 1 and 2 case/ch1.js
@@ -0,0 +1,4 @@
+var num = window.prompt("How many comics books do you buy every time you visit a comic book store?");
+var num2 = window.prompt("How many times a month do you visit a comic book store?");
+var ans = num + num2
+document.write( "You buy " + ans + " comic books a month.");
